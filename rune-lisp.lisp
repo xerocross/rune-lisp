efbbe9cfee14659121da156268d74c7feef98b70
Adam Cross <adamfgcross@gmail.com>
2025-09-20 17:40:42 -0400

    add updates to rune

--- rune-lisp.lisp
+++ rune-lisp.lisp
@@ -51,6 +51,23 @@
 
 
 
+(defparameter *hash-helper-registry-2* (make-hash-table :test 'eq))
+
+
+
+
+
+(defmacro define-hash-verb (name (h &rest args) docstring &body body)
+  "Register a hash-helper verb NAME with a handler (H &rest ARGS) and DOCSTRING."
+  `(progn
+     (setf (gethash ,name *hash-helper-registry*)
+           (list (lambda (,h &rest ,args) ,@body)
+                 ,docstring))
+     ',name))
+
+(define-hash-verb :get (h key) "Lookup KEY; returns value and present?"
+  (gethash key h))
+
 (defun hash-helper (hasht message &rest rest)
   (let ((keyword (intern (string-upcase message) :keyword)))
      (case keyword
@@ -61,19 +78,84 @@
        ((:help) (list :set :get :is :keys :help))
        (otherwise "did not understand"))))
 
+;; Internal (not exported) variable by convention uses earmuffs
+(defparameter *hash-helper-registry* (make-hash-table :test 'eq))
+(pdef (make-hash-table) -> *hash-helper-help*)
+
+(defun set-hash (hasht key value)
+  (setf (gethash key hasht) value))
+
+(set-hash *hash-helper-help* :set "usage `hash-obj :set key value`: sets the specified key-value pair")
 
 
+(set-hash *hash-helper-help* :keys "usage `hash-ob :keys`: returns a list of the hash table's keys")
+
+(defun define-hash-helper (key lambda-func help-text)
+  (set-hash *hash-helper-registry* key (list lambda-func help-text)))
+
+(define-hash-helper :set
+    (lambda (hasht rest) (>> ((key value) :from rest) (set-hash hasht key value)))
+  "usage `hash-obj :set key value`: sets the specified key-value pair")
+
+(define-hash-helper :get
+    (lambda (hasht rest) (>> ((key) :from rest) (gethash hasht key)))
+ "usage `hash-obj :set key value`: sets the specified key-value pair")
+
+(define-hash-helper :size
+    (lambda (hasht rest) (declare (ignore rest)) (hash-table-count hasht))
+ "usage `hash-obj :size`: get the number of pairs in the hash table")
+
+(defun get-hash-helper-map ()
+  (let ((funcs (make-hash-table))
+	(help-doc-list NIL)
+	(set-func NIL)
+	(get-func NIL))
+
+
+    
+    (setf set-func (lambda (hasht rest)
+		     (>> ((key value) :from rest)
+			 
+    (set-hash funcs :set (list set-func (gethash :set *hash-helper-help*)))
+    (setf get-func (lambda (hasht rest) (declare (ignore rest)) (hash-keys hasht)))
+    (setf (gethash :keys funcs) (list get-func
+    				      "usage `hash-ob :keys`: returns a list of the hash table's keys"))
+    (setf (gethash :help funcs) (list (lambda (hasht rest) (declare (ignore hasht) (ignore rest)) (dolist (x help-doc-list) (format t x)))
+    				      "usage `hash-ob :help`: prints this help info"))
+    (list funcs help-doc-list)
+    ))
+
+(defun build-hash-helper-docs (hash-helper-registry help-doc-list)
+  (maphash (lambda (k v)
+	       (setf help-doc-list (push (format NIL "~s ; ~a~%" k (second v)) help-doc-list)))
+	     hash-helper-registry))
 
 (defun hash-helper2 (hasht message &rest rest)
   (let ((keyword (intern (string-upcase message) :keyword))
-	(funcs (make-hash-table))
 	(func-obj NIL))
-    (setf (gethash :set funcs) (lambda (hasht rest) (>> ((key value) :from rest)
-							(hash-set hasht key value))))
-    (setf func-obj (gethash keyword funcs))
-    (if (not func-obj)
-	(format t "keyword ~s not understood" keyword)
-	(apply func-obj (list hasht rest)))))
+
+    
+    (>> ((funcs help-doc-list) :from (get-hash-helper-map))
+	(build-hash-helper-docs funcs help-doc-list)
+	(setf func-obj (first (gethash keyword funcs)))
+	(if (not func-obj)
+	    (format t "keyword ~s not understood" keyword)
+	    (apply func-obj (list hasht rest))))))
+
+
+(defun hash-helper3 (hasht message &rest rest)
+  (let ((keyword (intern (string-upcase message) :keyword))
+	(func-obj NIL)
+	(hash-helper-map *hash-helper-registry*)
+	(help-doc-list NIL))
+	(build-hash-helper-docs hash-helper-map help-doc-list)
+    (setf func-obj (first (gethash keyword hash-helper-map)))
+    (if (eq keyword :help)
+	(dolist (x help-doc-list) (format t x))
+	(if (not func-obj)
+	    (format t "keyword ~s not understood" keyword)
+	    (apply func-obj (list hasht rest))))))
+	
 
 
 (defun $ (lst)
